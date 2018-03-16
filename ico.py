15fdc30d95a2f0d67960b8b794154629396aec13
admin <longzsc88@163.com>
2018-03-16 09:56:15 +0800

    图片格式转换
    软件新版图标制作

---
+++ ico.py
@@ -0,0 +1,19 @@
+﻿#author: zhousc
+#email:zhousc88@gmail.com
+#图片制作成ICO
+
+import os
+import sys
+from PIL import Image
+
+if __name__=='__main__':
+    path=r'C:\Users\Administrator\Desktop\Export'
+    files=os.listdir(path)
+    size=256
+    for file in files:
+        filename=os.path.join(path,file)
+        name=file.split('.')[:-1].pop()
+        iconame=name+'.ico'
+        print(iconame)
+        f=Image.open(filename)
+        f.resize((size,size),Image.ANTIALIAS).save(os.path.join(path,iconame))
